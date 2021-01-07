f7eee23a7846665abf6b4dd5c000b9d5a97d7e3b
Michele Galasso <m.galasso@yandex.com>
2021-01-07 16:48:45 +0300

    added ase directory

---
+++ ase/run_vasp.py
@@ -0,0 +1,9 @@
+import os
+
+# vasp 5 local
+load_mkl = 'source /home/mgalasso/intel/compilers_and_libraries_2019.5.281/linux/mkl/bin/mklvars.sh intel64'
+load_mpi = 'source /home/mgalasso/intel/compilers_and_libraries_2019.5.281/linux/mpi/intel64/bin/mpivars.sh intel64'
+exitcode = os.system('{}; {}; mpirun -n 16 /home/mgalasso/softs/vasp.5.4.4/bin/vasp_std'.format(load_mkl, load_mpi))
+
+# vasp 6 module
+# exitcode = os.system('module load intel/mkl-11.2.3 mpi/impi-5.0.3 vasp/6.1.1; mpirun -n 16 vasp_std')
