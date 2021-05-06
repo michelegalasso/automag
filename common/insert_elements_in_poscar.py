6a24055d9865c3d1e7cbd135233aac0974840a49
Michele Galasso <m.galasso@yandex.com>
2021-05-06 19:09:10 +0300

    added function insert_elements_in_poscar

---
+++ common/insert_elements_in_poscar.py
@@ -0,0 +1,24 @@
+"""
+automag.common.insert_elements_in_poscar
+========================================
+
+Script which submits collinear relaxations.
+
+.. codeauthor:: Michele Galasso <m.galasso@yandex.com>
+"""
+
+
+def insert_elements_in_poscar(poscar_string, composition, magnetic_atom):
+    to_replace = '  '
+    replace_with_1 = ''
+    replace_with_2 = ''
+    for element, abundance in composition.items():
+        if element.name == magnetic_atom:
+            to_replace += f'{int(abundance / 2)}   {int(abundance / 2)}   '
+        else:
+            to_replace += f'{int(abundance)}   '
+
+        replace_with_1 += f'{element.name:>3s} '
+        replace_with_2 += f'{int(abundance):3d} '
+
+    return poscar_string.replace(to_replace, replace_with_1 + '\n' + replace_with_2)
