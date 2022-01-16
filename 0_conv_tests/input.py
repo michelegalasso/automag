64d33c89edc8a61315d5e07d00c8b9ee82aed3e6
Michele Galasso <m.galasso@yandex.com>
2022-01-16 09:34:59 +0300

    improved comments in the input file for convergence tests

--- 0_conv_tests/input.py
+++ 0_conv_tests/input.py
@@ -26,11 +26,11 @@ params = {
 # choose the magnetic configuration to use for convergence tests (default FM-HS)
 # configuration = 6 * [4.0] + 6 * [-4.0] + 18 * [0.0]
 
-# choose the trial values for ENCUT (default from 400 to 1000 eV at steps of 10 eV)
+# choose the trial values for ENCUT (default from 500 to 1000 eV at steps of 10 eV)
 # encut_values = range(350, 610, 10)
 
-# choose the trial values for ENCUT (default from 400 to 1000 eV at steps of 10 eV)
+# choose the trial values for SIGMA (default 0.05, 0.1 and 0.2 eV)
 # sigma_values = [0.05, 0.1, 0.2]
 
-# choose the trial values for ENCUT (default from 400 to 1000 eV at steps of 10 eV)
+# choose the trial values for R_k (default from 20 to 100 Ang^-1 at steps of 10 Ang^-1)
 # kpts_values = range(20, 110, 10)
