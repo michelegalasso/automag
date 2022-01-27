a43decf4e5341b0597fe1fef74c01955ea8f7b04
Michele Galasso <m.galasso@yandex.com>
2022-01-27 20:44:06 +0300

    now the value of U is calculated and plotted from the printed charges

--- 1_lin_response/2_plot_results.py
+++ 1_lin_response/2_plot_results.py
@@ -15,17 +15,18 @@ from scipy import stats
 # increase matplotlib pyplot font size
 plt.rcParams.update({'font.size': 18})
 
-# perturbations used for the VASP runs
-perturbations = np.array([-0.08, -0.05, -0.02, 0.02, 0.05])
+data = np.loadtxt('charges.txt')
 
-# number of d electrons on the first Fe site
-nscf = np.array([5.857, 5.914, 5.974, 6.057, 6.121])
-scf = np.array([5.994, 6.003, 6.010, 6.019, 6.025])
+perturbations = data[:, 0]
+nscf = data[:, 1]
+scf = data[:, 2]
 
 plt.figure(figsize=(16, 9))
 
 slope_nscf, intercept_nscf, r_value_nscf, p_value_nscf, std_err_nscf = stats.linregress(perturbations, nscf)
 slope_scf, intercept_scf, r_value_scf, p_value_scf, std_err_scf = stats.linregress(perturbations, scf)
+
+print(f'U = {(1/slope_scf) - (1/slope_nscf):4.2f}')
 
 plt.plot(perturbations, nscf, 'ro', label=f'NSCF (slope {slope_nscf:.4f})')
 plt.plot(perturbations, scf, 'bo', label=f'SCF (slope {slope_scf:.4f})')
