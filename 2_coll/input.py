067028f73d4e68b96cc35285349e42f848a35148
Michele Galasso <m.galasso@yandex.com>
2022-05-21 18:17:16 +0200

    coll step refers now to Fe4O6

--- 2_coll/input.py
+++ 2_coll/input.py
@@ -1,5 +1,5 @@
 # name of the poscar file to use in the automag/geometries folder
-poscar_file = 'Fe-bcc_2x2x2.vasp'
+poscar_file = 'Fe2O3-alpha_primitive.vasp'
 
 # maximum supercell size for generating distinct magnetic configurations
 supercell_size = 1
@@ -13,26 +13,26 @@ params = {
     'setups': 'recommended',
     'prec': 'Accurate',
     'ncore': 4,
-    'encut': 470,
+    'encut': 820,
     'ediff': 1e-6,
     'ismear': 1,
     'sigma': 0.05,
     'nelm': 200,
-    'kpts': 50,
-    # 'lmaxmix': 4,
+    'kpts': 20,
+    'lmaxmix': 4,
     'lcharg': False,
     'lwave': False,
     'isym': 0,
-    # 'ldau': True,
-    # 'ldautype': 2,
-    # 'ldaul': [-1, 2, 2, -1],
-    # 'ldauu': [0, 6.64, 6.76, 0],
-    # 'ldauj': [0, 0, 0, 0],
-    # 'ldauprint': 2,
+    'ldau': True,
+    'ldautype': 2,
+    'ldaul': [2, -1],
+    'ldauu': [3.75, 0],
+    'ldauj': [0, 0],
+    'ldauprint': 2,
 }
 
 # choose the atomic types to be considered magnetic (default transition metals)
 # magnetic_atoms = ['Mn']
 
 # specify a cutoff for considering only high-spin configurations
-hs_cutoff = 1.7
+# hs_cutoff = 1.7
