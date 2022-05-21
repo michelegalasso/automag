2ad488b939ec965c0bf97b7ab98a8dae0e8db88e
Michele Galasso <m.galasso@yandex.com>
2022-05-21 22:22:50 +0200

    input structure is not Ca3MnCoO6

--- 2_coll/input.py
+++ 2_coll/input.py
@@ -1,8 +1,8 @@
 # name of the poscar file to use in the automag/geometries folder
-poscar_file = 'Ni3TeO6_primitive.vasp'
+poscar_file = 'Ca3MnCoO6_primitive.vasp'
 
 # maximum supercell size for generating distinct magnetic configurations
-supercell_size = 2
+supercell_size = 1
 
 # choose the absolute value given to up and down spins
 spin_value = 4
@@ -13,7 +13,7 @@ params = {
     'setups': 'recommended',
     'prec': 'Accurate',
     'ncore': 4,
-    'encut': 830,
+    'encut': 820,
     'ediff': 1e-6,
     'ismear': 1,
     'sigma': 0.05,
@@ -25,9 +25,9 @@ params = {
     'isym': 0,
     'ldau': True,
     'ldautype': 2,
-    'ldaul': [2, -1],
-    'ldauu': [5.17, 0],
-    'ldauj': [0, 0],
+    'ldaul': [-1, 2, 2, -1],
+    'ldauu': [0, 6.64, 6.76, 0],
+    'ldauj': [0, 0, 0, 0],
     'ldauprint': 2,
 }
 
