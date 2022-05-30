6722eb1b6d1d36cfbf54d504c06ce0863850f2ae
Michele Galasso <m.galasso@yandex.com>
2022-05-30 23:07:51 +0200

    improved the plotting of results in coll step

--- 2_coll/2_plot_results.py
+++ 2_coll/2_plot_results.py
@@ -24,12 +24,6 @@ from pymatgen.symmetry.analyzer import SpacegroupAnalyzer
 if 'lower_cutoff' not in globals():
     lower_cutoff = 0
 
-# increase matplotlib pyplot font size
-plt.rcParams.update({'font.size': 20})
-
-# set figure size
-plt.figure(figsize=(16, 9))
-
 # create an ase atoms object
 path_to_poscar = '../geometries/' + poscar_file
 atoms = read(path_to_poscar)
@@ -60,6 +54,7 @@ with open(output_file, 'rt') as f:
 
 data = {}
 setting = 1
+not_found = []
 while os.path.isfile(f'trials/configurations{setting:03d}.txt'):
     with open(f'trials/configurations{setting:03d}.txt', 'rt') as f:
         for line in f:
@@ -71,19 +66,23 @@ while os.path.isfile(f'trials/configurations{setting:03d}.txt'):
                     'init_spins': [int(item) for item in values[1:]],
                 }
                 data[init_state] = dct
+            else:
+                not_found.append(init_state)
     setting += 1
 
 # keep the maximum value of setting
 max_setting = copy(setting)
 
 # extract the results
+red = []
+not_converged = []
 all_final_magmoms = []
 for line, maginfo in zip(lines, maginfos):
     values = line.split()
     init_state = values[0]
 
     if values[2].split('=')[1] == 'NONCONVERGED':
-        print(f'WARNING: energy calculation of {values[0]} did not converge and will be excluded from the graph.')
+        not_converged.append(init_state)
         del data[init_state]
     else:
         initial, final = maginfo.split('final_magmoms=')
@@ -97,7 +96,7 @@ for line, maginfo in zip(lines, maginfos):
         # exclude low-spin configurations
         flag = False
         mask = np.nonzero(initial)
-        if np.all(np.abs(final[mask]) > lower_cutoff) or values[0] == 'nm':
+        if np.all(np.abs(final[mask]) > lower_cutoff) or init_state == 'nm':
             flag = True
 
         magnification = len(initial) // sum(multiplicities)
@@ -122,9 +121,19 @@ for line, maginfo in zip(lines, maginfos):
             data[init_state]['kept_magmoms'] = True
         else:
             data[init_state]['kept_magmoms'] = False
-            print(f'WARNING: {values[0]} did not keep the original magmoms and will be marked in red on the graph.')
+            red.append(init_state)
 
-        data[init_state]['energy'] = float(values[-1].split('=')[1]) / len(atoms)
+        data[init_state]['energy'] = float(values[-1].split('=')[1]) / len(initial)
+
+if len(red) != 0:
+    print(f"The following {len(red)} configuration(s) did not keep the original magmoms and will be marked in red "
+          f"on the graph: {', '.join(red)}")
+if len(not_converged) != 0:
+    print(f"The energy calculation of the following {len(not_converged)} configuration(s) did not converge and will "
+          f"not be shown on the graph: {', '.join(not_converged)}")
+if len(not_found) != 0:
+    print(f"The following {len(not_found)} configuration(s) reported an error during energy calculation and will "
+          f"not be shown on the graph: {', '.join(not_found)}")
 
 # plt.hist(np.abs(all_final_magmoms), bins=40)
 # plt.savefig('spin_distribution.png')
@@ -137,6 +146,8 @@ while setting < max_setting:
     current_states = []
     current_energies = []
     for init_state, value in data.items():
+        if 'energy' not in value:
+            not_found.append(init_state)
         if init_state != 'nm' and value['setting'] == setting and value['kept_magmoms']:
             current_states.append(np.sign(value['init_spins']).tolist())
             current_energies.append(value['energy'])
@@ -145,6 +156,8 @@ while setting < max_setting:
         final_states = current_states
         final_energies = current_energies
     setting += 1
+
+
 
 # write states to file
 with open(f'states{final_setting:03d}.txt', 'wt') as f:
@@ -174,32 +187,49 @@ energies *= 1000
 
 # normalize energies for plot
 energies -= min(energies)
-toplim = max(energies) * 1.12
+toplim = max(energies) * 1.15
+bottomlim = -0.1 * max(energies)
+
 energies += 0.1 * max(energies)
+x_axis = np.arange(1, len(energies) + 1)
 
-# plot results
-repr_configurations = np.arange(1, len(energies) + 1)
-plt.bar(repr_configurations[~kept_magmoms], energies[~kept_magmoms], bottom=-0.1 * max(energies), color='r')
-plt.bar(repr_configurations[kept_magmoms], energies[kept_magmoms], bottom=-0.1 * max(energies), color='b')
+# split into chunks if too many configurations
+n_chunks = len(energies) // 14
+x_axis_split = np.array_split(x_axis, n_chunks)
+energies_split = np.array_split(energies, n_chunks)
+kept_magmoms_split = np.array_split(kept_magmoms, n_chunks)
+bar_labels_split = np.array_split(bar_labels, n_chunks)
 
-# label bars
-ax = plt.gca()
-rects = ax.patches
-rects.sort(key=lambda x: x.get_x())
-for bar_label, rect in zip(bar_labels, rects):
-    height = rect.get_height()
-    ax.text(rect.get_x() + rect.get_width() / 2, height - 0.09 * max(energies), bar_label,
-            fontsize='small', ha='center', va='bottom', rotation='vertical')
+for i, (X, Y, kept_magmoms_chunk, bar_labels_chunk) in \
+        enumerate(zip(x_axis_split, energies_split, kept_magmoms_split, bar_labels_split)):
+    # increase matplotlib pyplot font size
+    plt.rcParams.update({'font.size': 20})
 
-# label axes
-plt.xlabel('configurations')
-plt.ylabel(f'relative energy (meV/atom)')
-plt.xticks(np.arange(1, len(rects) + 1))
-plt.ylim(top=toplim)
+    # set figure size
+    plt.figure(figsize=(16, 9))
 
-# save or show bar chart
-plt.savefig('stability.png', bbox_inches='tight')
-# plt.show()
+    # plot results
+    plt.bar(X[~kept_magmoms_chunk], Y[~kept_magmoms_chunk], bottom=bottomlim, color='r')
+    plt.bar(X[kept_magmoms_chunk], Y[kept_magmoms_chunk], bottom=bottomlim, color='b')
+
+    # label bars
+    ax = plt.gca()
+    rects = ax.patches
+    rects.sort(key=lambda x: x.get_x())
+    for bar_label, rect in zip(bar_labels_chunk, rects):
+        height = rect.get_height()
+        ax.text(rect.get_x() + rect.get_width() / 2, height + 0.9 * bottomlim, bar_label,
+                fontsize='small', ha='center', va='bottom', rotation='vertical')
+
+    # label axes
+    plt.xlabel('configurations')
+    plt.ylabel(f'relative energy (meV/atom)')
+    plt.xticks(X)
+    plt.ylim(top=toplim)
+
+    # save or show bar chart
+    plt.savefig(f'stability{i + 1}.png', bbox_inches='tight')
+    plt.close()
 
 print(f'The most stable configuration is {bar_labels[np.argmin(energies)]}.')
 if np.logical_not(kept_magmoms[np.argmin(energies)]):
