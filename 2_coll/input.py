725ea23289278bc791fce05ad72c430c4a8bd01f
Michele Galasso <m.galasso@yandex.com>
2022-06-09 16:53:04 +0200

    moved to Ni3TeO6 and removed unnecessary parameters for structural relaxation

--- 2_coll/input.py
+++ 2_coll/input.py
@@ -1,11 +1,11 @@
 # name of the poscar file to use in the automag/geometries folder
-poscar_file = 'Fe2O3-alpha_conventional.vasp'
+poscar_file = 'Ni3TeO6_primitive.vasp'
 
 # maximum supercell size for generating distinct magnetic configurations
-supercell_size = 1
+supercell_size = 2
 
 # choose the absolute value given to up and down spins
-high_spin_value = 4
+high_spin_value = 3
 
 # if included all Wyckoff positions could be either HS or LS
 # low_spin_value = 1
@@ -16,16 +16,11 @@ params = {
     'setups': 'recommended',
     'prec': 'Accurate',
     'ncore': 4,
-    'encut': 820,
+    'encut': 830,
     'ediff': 1e-6,
-    'ediffg': -2e-3,
-    'ibrion': 2,
-    'isif': 3,
-    'nsw': 300,
-    'potim': 0.2,
     'ismear': 1,
     'sigma': 0.1,
-    'nelm': 80,
+    'nelm': 200,
     'kpts': 20,
     'lmaxmix': 4,
     'lcharg': False,
@@ -33,9 +28,9 @@ params = {
     'isym': 0,
     'ldau': True,
     'ldautype': 2,
-    'ldaul': [2, -1],
-    'ldauu': [3.75, 0],
-    'ldauj': [0, 0],
+    'ldaul': [2, -1, -1],
+    'ldauu': [5.17, 0, 0],
+    'ldauj': [0, 0, 0],
     'ldauprint': 2,
 }
 
