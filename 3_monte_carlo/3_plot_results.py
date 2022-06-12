97df81be9fadfcaf9dc0b4d07864e10116397f41
Michele Galasso <m.galasso@yandex.com>
2022-06-12 11:45:19 +0200

    now the critical exponent is also fitted with the help of scipy

--- 3_monte_carlo/3_plot_results.py
+++ 3_monte_carlo/3_plot_results.py
@@ -10,40 +10,33 @@ Script which plots the results of the Vampire run.
 import numpy as np
 import matplotlib.pyplot as plt
 
-
-def base(x, a):
-    return 1 - x/a
+from scipy.optimize import curve_fit
 
 
-exponent = 0.34
+def curve(x, Tc, beta):
+    return np.where(x < Tc, np.sign(1 - x/Tc) * np.abs(1 - x/Tc) ** beta, 0)
+
+
 filename = 'output'
 
 results = np.loadtxt(filename)
 temperature = results[:, 0]
 magnetization = results[:, -1]
 
-tc = 1
-trial = 1
-previous_residual = np.inf
-while trial < max(temperature):
-    bloch_curve = np.array([base(item, trial) ** exponent if base(item, trial) > 0 else 0 for item in temperature])
-    residual = np.sum(np.subtract(magnetization, bloch_curve) ** 2)
-    if residual < previous_residual:
-        tc = trial
-        previous_residual = residual
-    trial += 1
+pars, cov = curve_fit(f=curve, xdata=temperature, ydata=magnetization, p0=[900, 0.34])
 
-# Bloch curve for plot
-bloch_curve = np.array([base(item, tc) ** exponent if base(item, tc) > 0 else 0 for item in temperature])
+# curve for plot
+data_curve = curve(temperature, pars[0], pars[1])
 
 plt.rcParams.update({'font.size': 19})
 plt.figure(figsize=(8, 6))
 plt.plot(temperature, magnetization, label='Monte Carlo')
-plt.plot(temperature, bloch_curve, label='theory')
+plt.plot(temperature, data_curve, label='analytical fit')
 plt.xlabel('Temperature (K)')
 plt.ylabel('Mean magnetization length')
 plt.legend()
 # plt.show()
 plt.savefig('magnetization.png')
 
-print(f'The estimated critical temperature is {tc} K.')
+print(f'Fitted critical exponent beta = {pars[1]:.2f}')
+print(f'Fitted critical temperature Tc = {pars[0]:.0f} K')
