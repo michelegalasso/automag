0225f76a6877e9913304b2db03953b834ba18558
Michele Galasso <galasmic@cvut.cz>
2025-11-04 19:39:12 +0800

    Extended both convergence tests scripts to account for the kgrid case

--- 0_conv_tests/2_plot_results.py
+++ 0_conv_tests/2_plot_results.py
@@ -7,101 +7,141 @@ Script which plots results of convergence tests.
 .. codeauthor:: Michele Galasso <m.galasso@yandex.com>
 """
 
-from input import mode, poscar_file
-
 import os
-import numpy as np
+import matplotlib
+
 import matplotlib.pyplot as plt
-
-from ase.io import read
-
-# increase matplotlib pyplot font size
-plt.rcParams.update({'font.size': 20})
-
-# create an ase atoms object
-path_to_poscar = '../geometries/' + poscar_file
-atoms = read(path_to_poscar)
-
-# minimum and maximum values on the x axis for plotting, can be omitted
-# XMIN = 250
-# XMAX = 1000
+import numpy as np
 
 
-def single_plot(X, Y, label=None):
-    X = np.array(X, dtype=int)
-    Y = np.array(Y, dtype=float) / len(atoms)
-
-    # plot only between XMIN and XMAX
-    if 'XMAX' in globals() and 'XMIN' in globals():
-        indices = np.where(np.logical_and(X >= XMIN, X <= XMAX))
-        X = X[indices]
-        Y = Y[indices]
-
-    # sort the arrays
-    indices = np.argsort(X)
-    X = X[indices]
-    Y = Y[indices]
-
-    for x, y in zip(X[:-1], Y[:-1]):
-        if abs(y - Y[-1]) < 0.001:
-            if mode == 'encut':
-                print(f'ENCUT = {x} eV gives an error of less than 1 meV/atom w. r. t. the most accurate result.')
-            else:
-                print(f'kpts = {x} eV with {label} gives an error of less than 1 meV/atom w. r. t. '
-                      f'the most accurate result.')
-            break
-
-    # plot
-    if label is None:
-        plt.plot(X, Y, 'o')
-    else:
-        plt.plot(X, Y, 'o-', label=label)
+# matplotlib backend and font size
+matplotlib.use("TkAgg")
+plt.rcParams.update({"font.size": 14})
 
 
-# set figure size
-plt.figure(figsize=(16, 9))
+def read_energy_from_outcar(filename):
+    # initialization
+    result = None
 
-# read only lines which do not start with space
-lines = []
-calcfold = os.path.join(os.environ.get('AUTOMAG_PATH'), 'CalcFold')
-with open(os.path.join(calcfold, f"{atoms.get_chemical_formula(mode='metal', empirical=True)}_{mode}.txt"), 'r') as f:
-    for line in f:
-        if line[0] != ' ':
-            lines.append(line)
+    # read energy (sigma -> 0)
+    with open(filename) as f:
+        for line in f:
+            if "energy  without entropy" in line:
+                result = float(line.split()[-1])
 
-# extract the results
-params, energies, = [], []
-for line in lines:
-    values = line.split()
-    params.append(values[0].strip(mode))
-    energies.append(values[-1].split('=')[1])
+    # check that the energy value has been read
+    assert result is not None
 
-if mode == 'encut':
-    single_plot(params, energies)
+    # return energy value
+    return result
 
-elif mode == 'kgrid':
-    sigmas, kptss = [], []
-    for param in params:
-        sigma, kpts = param.split('-')
-        sigmas.append(sigma)
-        kptss.append(kpts)
 
-    sigmas = np.array(sigmas)
-    kptss = np.array(kptss)
-    energies = np.array(energies)
-    sigma_values = sorted(set(sigmas))
-    for sigma_value in sigma_values:
-        indices = np.where(sigmas == sigma_value)
-        single_plot(kptss[indices], energies[indices], label=f'SIGMA = {sigma_value}')
+### START OF INPUT PART ###
+
+# choose the desired mode: 'encut' or 'kgrid'
+mode = "kgrid"
+
+# root dir for the convergence test
+calc_dir = "/home/michele/EXCHANGE/Tb4H23/1_conv_tests/kgrid"
+
+### END OF INPUT PART ###
+
+# consistency check
+assert mode in ["encut", "kgrid"]
+
+if mode == "encut":
+    # initialization
+    encut_values = []
+    energy_values = []
+
+    for i, folder in enumerate(sorted(os.listdir(calc_dir))):
+        if i == 0:
+            # fetch number of atoms in the unit cell
+            with open(os.path.join(calc_dir, folder, "POSCAR"), "r") as f:
+                for j, line in enumerate(f):
+                    if j == 6:
+                        natoms_list = line.split()
+                        natoms = sum([int(item) for item in natoms_list])
+
+        if folder == "raw_input":
+            continue
+
+        encut = float(folder)
+        energy_atom = read_energy_from_outcar(os.path.join(calc_dir, folder, "OUTCAR")) / natoms
+
+        encut_values.append(encut)
+        energy_values.append(energy_atom)
+
+    # encut_values = np.array([300, 350, 400, 450, 500, 550, 600, 650, 700, 750])
+    # energy_values = np.array([-208.46069675, -208.78697811, -209.04029607, -209.16239793, -209.27367880, -209.33598966,
+    #                           -209.37279351, -209.38261711, -209.38414901, -209.38484983]) / 54
+
+    plt.plot(encut_values, energy_values, marker=".")
+    plt.xlabel("ENCUT")
+    plt.ylabel("Energy [eV/atom]")
+    # plt.savefig("ENCUT.png", bbox_inches="tight")
+    plt.tight_layout()
+    plt.show()
+
 else:
-    raise ValueError('MODEs currently implemented: encut, kgrid.')
+    # initialization
+    kpoints_values = []
+    sigma_values = []
+    energy_values = []
 
-if mode == 'encut':
-    plt.xlabel('ENCUT (eV)')
-else:
-    plt.xlabel(r'$R_k$')
-    plt.legend()
+    for i, folder in enumerate(sorted(os.listdir(calc_dir))):
+        if i == 0:
+            # fetch number of atoms in the unit cell
+            with open(os.path.join(calc_dir, folder, "POSCAR"), "r") as f:
+                for j, line in enumerate(f):
+                    if j == 6:
+                        natoms_list = line.split()
+                        natoms = sum([int(item) for item in natoms_list])
 
-plt.ylabel('energy (eV/atom)')
-plt.savefig(f'{mode}.png', bbox_inches='tight')
-# plt.show()
+        if folder == "raw_input":
+            continue
+
+        kpoints_string, sigma_string = folder.split("_")
+
+        kpoints_number = 1
+        for char in kpoints_string:
+            kpoints_number *= int(char)
+
+        energy_atom = read_energy_from_outcar(os.path.join(calc_dir, folder, "OUTCAR")) / natoms
+
+        kpoints_values.append(kpoints_number)
+        sigma_values.append(float("0." + sigma_string[1:]))
+        energy_values.append(energy_atom)
+
+    # go to numpy arrays
+    kpoints = np.array(kpoints_values)
+    sigmas = np.array(sigma_values)
+    energies = np.array(energy_values)
+
+    # get unique sigma values
+    unique_sigmas = np.unique(sigmas)
+
+    # create the plot
+    plt.figure()
+
+    for sigma in unique_sigmas:
+        # select data for this sigma
+        mask = sigmas == sigma
+        k = kpoints[mask]
+        e = energies[mask]
+
+        # Sort by kpoints for proper line plotting
+        sort_idx = np.argsort(k)
+        k_sorted = k[sort_idx]
+        e_sorted = e[sort_idx]
+
+        plt.plot(np.cbrt(k_sorted), e_sorted, marker='o', label=f'σ = {sigma}')
+
+    # labels and legend
+    plt.xlabel('Mean k-points per direction')
+    plt.ylabel('Energy [eV/atom]')
+    plt.legend(title='Sigma')
+    plt.grid(True)
+    plt.savefig("KGRID.png", bbox_inches="tight")
+    # plt.tight_layout()
+    # plt.show()
