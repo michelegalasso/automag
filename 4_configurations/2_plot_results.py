76f23eab72f781a8ab403158fd2497b6a8b00c88
Michele Galasso <galasmic@cvut.cz>
2025-11-23 15:14:27 +0800

    Made a preliminary plotting script for magnetic configurations

--- 4_configurations/2_plot_results.py
+++ 4_configurations/2_plot_results.py
@@ -1,253 +1,139 @@
-"""
-automag.2_coll.2_plot_results
-=============================
-
-Script which plots results of magnetic relaxations.
-
-.. codeauthor:: Michele Galasso <m.galasso@yandex.com>
-"""
-
-from input import *
-
 import os
-import json
-import shutil
 import numpy as np
 import matplotlib.pyplot as plt
 
-from copy import copy
 from ase.io import read
-from pymatgen.core.structure import Structure
-from pymatgen.symmetry.analyzer import SpacegroupAnalyzer
+from pymatgen.io.vasp.inputs import Incar
+
+
+### START OF INPUT PART ###
+
+# root dir for the convergence test
+calc_dir = "/home/michele/EXCHANGE/Tb4H23/4_configurations"
+
+# number of heavy atoms in the unit cell (ignored if plotting magmom)
+n_heavy_atoms = 8
+
+### END OF INPUT PART ###
+
+
+def is_comment(line):
+    stripped_line = line.strip()
+    if stripped_line.startswith("!") or stripped_line.startswith("#"):
+        return True
+
+    return False
+
+
+def rewrite_incar(path_to_incar):
+    # initialization
+    new_line = ""
+
+    with open(path_to_incar, "r") as original_file:
+        with open(path_to_incar + "2", "w") as new_file:
+            for original_line in original_file:
+                parts = original_line.split()
+                if len(parts) > 0 and parts[-1].endswith("\\") and not is_comment(original_line):
+                    new_line += original_line.split("\\")[0]
+                    write_flag = False
+                else:
+                    new_line += original_line
+                    write_flag = True
+
+                if write_flag:
+                    new_file.write(new_line)
+                    new_line = ""
 
 
 def better_sort(state):
-    if state[0] == 'nm':
+    if state == 'nm':
         return 'aa'
-    elif state[0][:2] == 'fm':
-        return 'aaa' + f'{int(state[0][2:]):3d}'
+    elif state.startswith('fm'):
+        return 'aaa' + state[2:]
     else:
-        return state[0][:3] + f'{int(state[0][3:]):3d}'
+        return state
 
 
-# take care of the case when lower_cutoff has not been specified
-if 'lower_cutoff' not in globals():
-    lower_cutoff = 0
-
-# create an ase atoms object
-path_to_poscar = '../geometries/' + poscar_file
-atoms = read(path_to_poscar)
-
-# get the multiplicities of each Wyckoff position
-structure = Structure.from_file(path_to_poscar)
-analyzer = SpacegroupAnalyzer(structure)
-symmetrized_structure = analyzer.get_symmetrized_structure()
-multiplicities = np.array([len(item) for item in symmetrized_structure.equivalent_indices])
-
-# path to results file with data
-calcfold = os.path.join(os.environ.get('AUTOMAG_PATH'), 'CalcFold')
-output_file = os.path.join(calcfold, f"{atoms.get_chemical_formula(mode='metal', empirical=True)}_singlepoint.txt")
-
-# exit if no trials folder
-if not os.path.isdir('trials'):
-    raise IOError('No trials folder found.')
-
-# read lines
-lines, maginfos, presents = [], [], []
-with open(output_file, 'rt') as f:
-    for line in f:
-        if line[0] != ' ':
-            presents.append(line.split()[0])
-            lines.append(line)
-        else:
-            maginfos.append(line)
-
-data = {}
-setting = 1
-not_found = []
-while os.path.isfile(f'trials/configurations{setting:03d}.txt'):
-    with open(f'trials/configurations{setting:03d}.txt', 'rt') as f:
-        for line in f:
-            values = line.split()
-            init_state = values[0]
-            if init_state in presents:
-                dct = {
-                    'setting': setting,
-                    'init_spins': [int(item) for item in values[1:]],
-                }
-                data[init_state] = dct
-            else:
-                not_found.append(init_state)
-    setting += 1
-
-# keep the maximum value of setting
-max_setting = copy(setting)
-
-# extract the results
-red = []
-not_converged = []
-all_final_magmoms = []
-for line, maginfo in zip(lines, maginfos):
-    values = line.split()
-    init_state = values[0]
-
-    if values[-4].split('=')[1] == 'NONCONVERGED':
-        not_converged.append(init_state)
-        del data[init_state]
-    else:
-        initial, final = maginfo.split('final_magmoms=')
-        initial = initial[initial.index('[') + 1:initial.index(']')]
-        final = final[final.index('[') + 1:final.index(']')]
-        initial = np.array(initial.split(), dtype=float)
-        final = np.array(final.split(), dtype=float)
-
-        all_final_magmoms.extend(final.tolist())
-
-        # exclude low-spin configurations
-        flag = False
-        mask = np.nonzero(initial)
-        if np.all(np.abs(final[mask]) > lower_cutoff) or init_state == 'nm':
-            flag = True
-
-        magnification = len(initial) // sum(multiplicities)
-        current_multiplicities = magnification * multiplicities
-
-        start = 0
-        kept = True
-        for multiplicity in current_multiplicities:
-            w_initial = initial[start:start + multiplicity]
-            w_final = final[start:start + multiplicity]
-            start += multiplicity
-
-            if not np.any(w_initial):
-                if np.any(np.around(w_final)):
-                    kept = False
-            else:
-                prod = np.multiply(np.sign(w_initial), w_final)
-                if prod.max() - prod.min() > 0.08:
-                    kept = False
-
-        if kept and flag:
-            data[init_state]['kept_magmoms'] = True
-        else:
-            data[init_state]['kept_magmoms'] = False
-            red.append(init_state)
-
-        data[init_state]['energy'] = float(values[-1].split('=')[1]) / len(initial)     # energy per atom
-
-if len(red) != 0:
-    print(f"The following {len(red)} configuration(s) did not keep the original magmoms and will be marked in red "
-          f"on the graph: {', '.join(red)}")
-if len(not_converged) != 0:
-    print(f"The energy calculation of the following {len(not_converged)} configuration(s) did not converge and will "
-          f"not be shown on the graph: {', '.join(not_converged)}")
-if len(not_found) != 0:
-    print(f"The following {len(not_found)} configuration(s) reported an error during energy calculation and will "
-          f"not be shown on the graph: {', '.join(not_found)}")
-
-# plt.hist(np.abs(all_final_magmoms), bins=40)
-# plt.savefig('spin_distribution.png')
-
-setting = 1
-final_states = []
-final_setting = setting
-final_energies = []
-final_min = np.inf
-while setting < max_setting:
-    current_states = []
-    current_energies = []
-    for init_state, value in data.items():
-        if init_state != 'nm' and value['setting'] == setting and value['kept_magmoms']:
-            current_states.append(np.sign(value['init_spins']).tolist())
-            current_energies.append(value['energy'])
-    if min(current_energies) < final_min:
-        final_setting = setting
-        final_states = current_states
-        final_energies = current_energies
-        final_min = min(current_energies)
-    setting += 1
-
-# filter out configurations containing NM states
-tc_states = []
-tc_energies = []
-for state, energy in zip(final_states, final_energies):
-    if 0 not in state:
-        tc_states.append(state)
-        tc_energies.append(energy)
-
-# write states to file
-with open(f'states{final_setting:03d}.txt', 'wt') as f:
-    json.dump(tc_states, f)
-
-# write energies to file
-with open(f'energies{final_setting:03d}.txt', 'wt') as f:
-    json.dump(tc_energies, f)
-
-# copy setting file with geometry
-shutil.copy(f'trials/setting{final_setting:03d}.vasp', '.')
-
-# extract values for plot
-bar_labels = []
+# initialization
+mag_states = []
 energies = []
-kept_magmoms = []
-for key, value in sorted(data.items(), key=better_sort):
-    bar_labels.append(key)
-    energies.append(value['energy'])
-    kept_magmoms.append(value['kept_magmoms'])
+colors = []
 
-energies = np.array(energies)
-kept_magmoms = np.array(kept_magmoms)
+for folder in sorted(os.listdir(calc_dir), key=better_sort):
+    if folder != "raw_input" and folder != "full_relax":
+        # get around Pymatgen bug by rewriting the INCAR as INCAR2
+        rewrite_incar(os.path.join(calc_dir, folder, 'INCAR'))
 
-# energies from eV/atom to meV/atom
-energies *= 1000
+        # read INCAR2 and OUTCAR
+        incar = Incar.from_file(os.path.join(calc_dir, folder, 'INCAR2'))
+        atoms = read(os.path.join(calc_dir, folder, 'OUTCAR'))
+        energy = atoms.get_total_energy()
 
-# normalize energies for plot
-energies -= min(energies)
-toplim = max(energies) * 1.15
-bottomlim = -0.1 * max(energies)
+        # evaluate whether magnetic moments significantly changed
+        initial_magmom = np.array(incar.get("MAGMOM"))
+        final_magmom = atoms.get_magnetic_moments()
 
-energies += 0.1 * max(energies)
-x_axis = np.arange(1, len(energies) + 1)
+        # the collinear case has not been implemented yet
+        if initial_magmom.shape[1] != 3 or final_magmom.shape[1] != 3:
+            raise NotImplementedError("The collinear case has not been implemented yet.")
 
-# split into chunks if too many configurations
-n_chunks = len(energies) // 14 + 1
-x_axis_split = np.array_split(x_axis, n_chunks)
-energies_split = np.array_split(energies, n_chunks)
-kept_magmoms_split = np.array_split(kept_magmoms, n_chunks)
-bar_labels_split = np.array_split(bar_labels, n_chunks)
+        # compute the norms
+        initial_magmom_norms = np.linalg.norm(initial_magmom, axis=1)
+        final_magmom_norms = np.linalg.norm(final_magmom, axis=1)
 
-for i, (X, Y, kept_magmoms_chunk, bar_labels_chunk) in \
-        enumerate(zip(x_axis_split, energies_split, kept_magmoms_split, bar_labels_split)):
-    # increase matplotlib pyplot font size
-    plt.rcParams.update({'font.size': 20})
+        print("\n")
+        for j, item in enumerate(final_magmom):
+            if j < 8:
+                print(f"{item[0]} {item[1]} {item[2]}")
 
-    # set figure size
-    plt.figure(figsize=(16, 9))
+        # blue means no big variation, red means big variation, orange means angle variation
+        color = "tab:blue"
+        indices = []
 
-    # plot results
-    plt.bar(X[~kept_magmoms_chunk], Y[~kept_magmoms_chunk], bottom=bottomlim, color='r')
-    plt.bar(X[kept_magmoms_chunk], Y[kept_magmoms_chunk], bottom=bottomlim, color='b')
+        for i, (initial_norm, final_norm) in enumerate(zip(initial_magmom_norms, final_magmom_norms)):
+            # if something initialized as zero goes crazy high we signal a big variation
+            if np.isclose(initial_norm, 0.0) and final_norm > 0.5:
+                color = "tab:red"
 
-    # label bars
-    ax = plt.gca()
-    rects = ax.patches
-    rects = sorted(rects, key=lambda x: x.get_x())
-    for bar_label, rect in zip(bar_labels_chunk, rects):
-        height = rect.get_height()
-        ax.text(rect.get_x() + rect.get_width() / 2, height + 0.85 * bottomlim, bar_label,
-                fontsize='medium', ha='center', va='bottom', rotation='vertical')
+            if initial_norm > 0.5:
+                # if something initialized high goes below 0.5 we signal a big variation
+                if final_norm < 0.5:
+                    color = "tab:red"
 
-    # label axes
-    plt.xlabel('configurations')
-    plt.ylabel(f'relative energy (meV/atom)')
-    plt.xticks(X)
-    plt.ylim(top=toplim)
+                # otherwise we write down the index for later analyzing the angle
+                else:
+                    indices.append(i)
 
-    # save or show bar chart
-    plt.savefig(f'stability{i + 1:02d}.png', bbox_inches='tight')
-    plt.close()
+        # do this only if not flagged as big variation
+        if color != "tab:red":
+            # go to numpy array
+            indices = np.array(indices)
 
-print(f'The most stable configuration is {bar_labels[np.argmin(energies)]}.')
-if np.logical_not(kept_magmoms[np.argmin(energies)]):
-    print('WARNING: values of initial and final magnetic moments of the most stable configuration '
-          'significantly differ.')
+            # get the angle between initial and final magmom vectors in degrees
+            dot_products = np.sum(initial_magmom[indices] * final_magmom[indices], axis=1)
+            norms = initial_magmom_norms[indices] * final_magmom_norms[indices]
+            cos_thetas = np.clip(dot_products / norms, -1.0, 1.0)
+            angles_deg = np.degrees(np.arccos(cos_thetas))
+
+            if angles_deg.max() > 5.0:
+                color = "tab:orange"
+
+        mag_states.append(folder)
+        energies.append(energy)
+        colors.append(color)
+
+# numpy arrays
+mag_states = np.array(mag_states)
+energies = np.array(energies) / n_heavy_atoms
+
+# energy differences
+energies = energies - energies.min()
+
+# plot
+plt.figure(figsize=(16, 9))
+plt.bar(mag_states, energies + 1, bottom=-1, color=colors)
+
+plt.xticks(rotation=90)
+# plt.ylim(-0.01, 0.03)
+plt.ylabel('Energy [eV/atom]')
+plt.show()
